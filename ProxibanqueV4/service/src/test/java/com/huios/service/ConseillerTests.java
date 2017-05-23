3d6fa8bf46915368a4ce3b569809fa75c991ebab
MsPeem <perrine.emin@gmail.com>
2017-05-23 17:10:24 +0200

    Préparation pour tests service

---
+++ ProxibanqueV4/service/src/test/java/com/huios/service/ConseillerTests.java
@@ -0,0 +1,14 @@
+package com.huios.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConseillerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
